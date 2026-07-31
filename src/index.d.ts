4c85141c2bca34710179f02a68433dcb84033c5c
Leszek Kicior <kiciorleszek@gmail.com>
2026-07-31 10:36:40 +0200

    maxRank prop to control maximum node level

--- src/index.d.ts
+++ src/index.d.ts
@@ -63,6 +63,8 @@ declare class SunburstChart {
   minSliceAngle(degrees: number): SunburstChart;
   maxLevels(): number;
   maxLevels(degrees: number): SunburstChart;
+  maxRank(): number;
+  maxRank(rank: number): SunburstChart;
   excludeRoot(): boolean;
   excludeRoot(exclude: boolean): SunburstChart;
   centerRadius(): number;
